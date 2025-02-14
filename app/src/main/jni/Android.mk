05da5bfeff68191d67df378f9743fde6f0eabddc
ayuyas <66871070+ayuyas@users.noreply.github.com>
2025-02-14 17:32:16 +0800

    更新 Android.mk

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -1,3 +1,14 @@
 APP_ABI = armeabi-v7a
-include $(call all-subdir-makefiles)
+#include $(call all-subdir-makefiles)
+LOCAL_PATH := $(call my-dir)
+
+# 排除 subdirectory3
+subdirs := $(subst $(LOCAL_PATH)/,,$(patsubst %/,%,$(wildcard $(LOCAL_PATH)/*/)))
+subdirs := $(filter-out luagl/, $(subdirs))
+
+$(foreach dir,$(subdirs),\
+    include $(CLEAR_VARS)\
+    include $(dir)/Android.mk\
+)
+
 
